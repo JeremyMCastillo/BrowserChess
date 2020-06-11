e0d7e89cafa2ecbb14a3d9b95b98f5cfd749875e
Jeremy Castillo <jcast029.jc@gmail.com>
2020-06-10 21:28:45 -0700

    Refactoring Player to be a standard class.
    Signed-off-by: Jeremy Castillo <jcast029.jc@gmail.com>

--- models/players.js
+++ models/players.js
@@ -1,35 +1,16 @@
-const _ = require('lodash');
-
-const { mongoose } = require('../system/mongoose');
-
-var PlayerSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    minlength: 1,
-    trim: true
-  },
-  client: {
-    os: String,
-    osVersion: String,
-    appVersion: String,
-    brand: String,
-    carrier: String,
-    country: String,
-    locale: String,
-    manufacturer: String,
-    version: String,
-    notch: Boolean,
-    deviceType: String
+class Player {
+  // name: String - identifier used to tell players apart.
+  //                Should only be unique within the same Board.
+  // analytics: Object - contains location, ip and other tracking
+  //                     info that allows us to see who is playing.
+  constructor(name, analytics) {
+    this.name = name;
+    this.analytics = analytics;
   }
-});
 
-PlayerSchema.methods.toJSON = function () {
-  var player = this;
-  var playerObject = player.toObject();
-
-  return _.pick(playerObject, ['_id', 'email']);
-};
-
-var Player = mongoose.model('Player', PlayerSchema);
+  getName() {
+    return this.name;
+  }
+}
 
 module.exports = { Player };
