33fa65f2fbffc325dc861f20c9aa8957109b7716
derekdyer0309 <derekdyer02@gmail.com>
2020-06-10 21:54:34 -0700

    updated pieces.js with expected argument types

--- models/pieces.js
+++ models/pieces.js
@@ -1,4 +1,10 @@
 class Piece {
+  // type: String - identifier used to tell pieces apart.
+  //              - Should only be unique to the player on the same Board.
+  // color: String - Makes piece unique to the player by color
+  //                     
+  // movement: function that determines how the piece moves on the matrix;
+  // default null, updated with updateMove()
     constructor(type, color, movement) {
       this.type = type,
       this.color = color,
