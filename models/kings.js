0bc9046c0b6caebd2d34fa2a50bfe8cf541f8333
derekdyer0309 <derekdyer02@gmail.com>
2020-06-17 00:55:50 -0700

    finished kings.js and knights.js formatted with prettier

--- models/kings.js
+++ models/kings.js
@@ -1,5 +1,5 @@
-const { Piece, PieceType } = require('./pieces');
-const { Cell } = require('./cells');
+const { Piece, PieceType } = require("./pieces");
+const { Cell } = require("./cells");
 
 class King extends Piece {
   constructor(color, x, y) {
@@ -17,21 +17,74 @@ class King extends Piece {
     // is basic for all cardinal directions and diagonals for now
     for (let i = 0; i < 1; i++) {
       // one space forward
-      validMoves.push(new Cell(currentX + 1, currentY))
-      // one space diagonal left forward
-      validMoves.push(new Cell(currentX + 1, currentY - 1))
-      // one space diagonal right forward
-      validMoves.push(new Cell(currentX + 1, currentY + 1))
-      // one space backward
-      validMoves.push(new Cell(currentX - 1, currentY))
-      //one space diagonal left backward
-      validMoves.push(new Cell(currentX - 1, currentY - 1))
-      //one space diagonal right backward
-      validMoves.push(new Cell(currentX - 1, currentY + 1))
-      //one space right
-      validMoves.push(new Cell(currentX, currentY + 1))
-      //one space left
-      validMoves.push(new Cell(currentX, currentY - 1))
+      if (board.hasPieceAt(currentX + 1, currentY)) {
+        if (
+          board.matrix[currentX + 1][currentY].piece.getColor() !== this.color
+        ) {
+          validMoves.push(new Cell(currentX + 1, currentY));
+        }
+        break;
+        // one space diagonal left forward
+      } else if (board.hasPieceAt(currentX + 1, currentY - 1)) {
+        if (
+          board.matrix[currentX + 1][currentY + 1].piece.getColor() !==
+          this.color
+        ) {
+          validMoves.push(new Cell(currentX + 1, currentY - 1));
+        }
+        break;
+        // one space diagonal right forward
+      } else if (board.hasPieceAt(currentX + 1, currentY + 1)) {
+        if (
+          board.matrix[currentX + 1][currentY + 1].piece.getColor() !==
+          this.color
+        ) {
+          validMoves.push(new Cell(currentX + 1, currentY + 1));
+        }
+        break;
+        // one space backward
+      } else if (board.hasPieceAt(currentX - 1, currentY)) {
+        if (
+          board.matrix[currentX + 1][currentY].piece.getColor() !== this.color
+        ) {
+          validMoves.push(new Cell(currentX - 1, currentY));
+        }
+        break;
+        //one space diagonal left backward
+      } else if (board.hasPieceAt(currentX - 1, currentY - 1)) {
+        if (
+          board.matrix[currentX - 1][currentY - 1].piece.getColor() !==
+          this.color
+        ) {
+          validMoves.push(new Cell(currentX - 1, currentY - 1));
+        }
+        break;
+        //one space diagonal right backward
+      } else if (board.hasPieceAt(currentX - 1, currentY + 1)) {
+        if (
+          board.matrix[currentX - 1][currentY + 1].piece.getColor() !==
+          this.color
+        ) {
+          validMoves.push(new Cell(currentX - 1, currentY + 1));
+        }
+        break;
+        //one space right
+      } else if (board.hasPieceAt(currentX, currentY + 1)) {
+        if (
+          board.matrix[currentX][currentY + 1].piece.getColor() !== this.color
+        ) {
+          validMoves.push(new Cell(currentX, currentY + 1));
+        }
+        break;
+        //one space left
+      } else if (board.hasPieceAt(currentX, currentY - 1)) {
+        if (
+          board.matrix[currentX][currentY - 1].piece.getColor() !== this.color
+        ) {
+          validMoves.push(new Cell(currentX, currentY - 1));
+        }
+        break;
+      }
     }
     return validMoves;
   }
---
+++ models/knights.js
@@ -0,0 +1,94 @@
+const { Piece, PieceType } = require("./pieces");
+const { Cell } = require("./cells");
+
+class Knight extends Piece {
+  constructor(color, x, y) {
+    super(PieceType.knight, color);
+    this.x = x;
+    this.y = y;
+  }
+
+  getValidMoves(board) {
+    let currentX = this.x;
+    let currentY = this.y;
+    let validMoves = [];
+    for (let i = 0; i < 1; i++) {
+      // forward / left
+      if (board.hasPieceAt(currentX + 2, currentY - 1)) {
+        if (
+          board.matrix[currentX + 2][currentY - 1].piece.getColor() !==
+          this.color
+        ) {
+          validMoves.push(new Cell(currentX + 2, currentY - 1));
+        }
+        break;
+        // forward / right
+      } else if (board.hasPieceAt(currentX + 2, currentY + 1)) {
+        if (
+          board.matrix[currentX + 2][currentY + 1].piece.getColor() !==
+          this.color
+        ) {
+          validMoves.push(new Cell(currentX + 2, currentY + 1));
+        }
+        break;
+        // right / advance
+      } else if (board.hasPieceAt(currentX + 1, currentY + 2)) {
+        if (
+          board.matrix[currentX + 1][currentY + 2].piece.getColor() !==
+          this.color
+        ) {
+          validMoves.push(new Cell(currentX + 1, currentY + 2));
+        }
+        break;
+        // right /retreat
+      } else if (board.hasPieceAt(currentX - 1, currentY + 2)) {
+        if (
+          board.matrix[currentX - 1][currentY + 2].piece.getColor() !==
+          this.color
+        ) {
+          validMoves.push(new Cell(currentX - 1, currentY + 2));
+        }
+        break;
+        //back / right
+      } else if (board.hasPieceAt(currentX - 2, currentY + 1)) {
+        if (
+          board.matrix[currentX - 2][currentY + 1].piece.getColor() !==
+          this.color
+        ) {
+          validMoves.push(new Cell(currentX - 2, currentY + 1));
+        }
+        break;
+        //back / left
+      } else if (board.hasPieceAt(currentX - 2, currentY - 1)) {
+        if (
+          board.matrix[currentX - 2][currentY - 1].piece.getColor() !==
+          this.color
+        ) {
+          validMoves.push(new Cell(currentX - 2, currentY - 1));
+        }
+        break;
+        //left /advance
+      } else if (board.hasPieceAt(currentX + 1, currentY + 2)) {
+        if (
+          board.matrix[currentX + 1][currentY + 2].piece.getColor() !==
+          this.color
+        ) {
+          validMoves.push(new Cell(currentX + 1, currentY + 2));
+        }
+        break;
+        //left / retreat
+      } else if (board.hasPieceAt(currentX - 1, currentY - 2)) {
+        if (
+          board.matrix[currentX - 1][currentY - 2].piece.getColor() !==
+          this.color
+        ) {
+          validMoves.push(new Cell(currentX - 1, currentY - 2));
+        }
+        break;
+      }
+    }
+    return validMoves;
+  }
+}
+
+module.exports = { Knight };
