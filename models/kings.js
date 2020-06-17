b229e2d2c877193b759352f1358836cb2dfea537
derekdyer0309 <derekdyer02@gmail.com>
2020-06-17 00:15:10 -0700

    basic movment allowing king to move in all cardinal directions and diagonals present

--- models/kings.js
+++ models/kings.js
@@ -1,20 +1,37 @@
-import { Piece, PieceType } from "./pieces";
-import { Cell } from "./cells";
+const { Piece, PieceType } = require('./pieces');
+const { Cell } = require('./cells');
 
 class King extends Piece {
   constructor(color, x, y) {
-    super(PieceType.pawn, color);
+    super(PieceType.king, color);
     this.x = x;
     this.y = y;
   }
 
   getValidMoves(board) {
+    //TODO: Change this to only add the space if it wouldn't put the king in check
     let currentX = this.x;
     let currentY = this.y;
     let validMoves = [];
-    // logic for move
+    // basic logic for movement, since the king can only capture in specific ways this
+    // is basic for all cardinal directions and diagonals for now
     for (let i = 0; i < 1; i++) {
-      return null
+      // one space forward
+      validMoves.push(new Cell(currentX + 1, currentY))
+      // one space diagonal left forward
+      validMoves.push(new Cell(currentX + 1, currentY - 1))
+      // one space diagonal right forward
+      validMoves.push(new Cell(currentX + 1, currentY + 1))
+      // one space backward
+      validMoves.push(new Cell(currentX - 1, currentY))
+      //one space diagonal left backward
+      validMoves.push(new Cell(currentX - 1, currentY - 1))
+      //one space diagonal right backward
+      validMoves.push(new Cell(currentX - 1, currentY + 1))
+      //one space right
+      validMoves.push(new Cell(currentX, currentY + 1))
+      //one space left
+      validMoves.push(new Cell(currentX, currentY - 1))
     }
     return validMoves;
   }
