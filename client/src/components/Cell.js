6d536b09d8689c9b9041ab33618acc52572e96a6
derekdyer0309 <derekdyer02@gmail.com>
2020-07-13 18:48:19 -0700

    added piece type to class of cell

--- client/src/components/Cell.js
+++ client/src/components/Cell.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../index.css';
 
 export default function Square(props) {
-  console.log("This is from Cell.js: " , props)
+  // console.log("This is from Cell.js: " , props)
   let pieceClass = props.piece ? props.piece.color : '';
   let pieceType = props.piece ? props.piece.type: '';
   return (
