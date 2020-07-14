86cd8f5e3c5407e0465e5f350c660cc1ed0c9568
derekdyer0309 <derekdyer02@gmail.com>
2020-07-13 18:04:51 -0700

    board rendering

--- client/src/components/Graveyard.js
+++ client/src/components/Graveyard.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../index.css';
-import Cell from './Cell.js';
 
-export const Graveyard = () => {
-  return <div />;
+//TODO: add status to pieces
+
+export const Graveyard = (props) => {
+  let dead = [];
+  let pieceClass = props.piece ? props.piece.color: '';
+  let pieceType = props.piece ? props.piece.type: '';
+  // let pieceStatus = props.piece ? props.piece.status: '';
+  
+
+  // useEffect() = props = {
+  //   if (pieceStatus = 'dead') {
+  //     dead.push(pieceClass, pieceType);
+  //   }
+  // };
+
+  return <div className="graveyard">{dead}</div>;
 };
