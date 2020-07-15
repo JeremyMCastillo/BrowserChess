944742e192b483509be31506a1b8e18e9e0af24b
derekdyer0309 <derekdyer02@gmail.com>
2020-07-14 21:36:00 -0700

    selectPiece on Cell.js

--- client/src/components/Cell.js
+++ client/src/components/Cell.js
@@ -2,40 +2,36 @@ import React, { useState } from 'react';
 import '../index.css';
 
 
-
-
 export default function Square(props) {
 
-  const [selectedPiece, setSelectedPiece] = useState({});
 
   let pieceClass = props.piece ? props.piece.color : '';
   let pieceType = props.piece ? props.piece.type: '';
-  let active = false;
 
-  const togglePiece = e => {
+  const [selectedPiece, setSelectedPiece] = useState({
+    pieceClass,
+    pieceType,
+  });
+
+
+  const selectPiece = e => {
     e.preventDefault();
     setSelectedPiece({
       ...selectedPiece,
       pieceClass,
       pieceType,
-      active: !selectedPiece.active
     });
     console.log(selectedPiece)
   };
 
-
-  // console.log("This is from Cell.js: " , props)
-
   return (
-    <button className={
-    active 
-    ? `square piece column ${selectedPiece.pieceClass} ${selectedPiece.pieceType}` 
-    : `square piece column`} 
-    onClick={togglePiece}>
+    <button className={`square piece column`} 
+    onClick={selectPiece}>
       {
       props.piece 
       ? props.piece.type 
-      : selectedPiece.type}
+      : null}
     </button>
   );
-}
+};
+
