f9c624d26339687aa6d95c52ad4395ac8ea558cb
derekdyer0309 <derekdyer02@gmail.com>
2020-07-14 21:07:17 -0700

    began working on piece movement in Cell.js

--- client/src/components/Cell.js
+++ client/src/components/Cell.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../index.css';
 
+
+
+
 export default function Square(props) {
-  // console.log("This is from Cell.js: " , props)
+
+  const [selectedPiece, setSelectedPiece] = useState({});
+
   let pieceClass = props.piece ? props.piece.color : '';
   let pieceType = props.piece ? props.piece.type: '';
+  let active = false;
+
+  const togglePiece = e => {
+    e.preventDefault();
+    setSelectedPiece({
+      ...selectedPiece,
+      pieceClass,
+      pieceType,
+      active: !selectedPiece.active
+    });
+    console.log(selectedPiece)
+  };
+
+
+  // console.log("This is from Cell.js: " , props)
+
   return (
-    <button className={`square piece column ${pieceClass} ${pieceType}`}>
-      {props.piece ? props.piece.type : ''}
+    <button className={
+    active 
+    ? `square piece column ${selectedPiece.pieceClass} ${selectedPiece.pieceType}` 
+    : `square piece column`} 
+    onClick={togglePiece}>
+      {
+      props.piece 
+      ? props.piece.type 
+      : selectedPiece.type}
     </button>
   );
 }
