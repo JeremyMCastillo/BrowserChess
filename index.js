abe0a9ac35e9090251124949c4c10e3284d76f93
Jeremy Castillo <jeremy.c@accessorypower.com>
2020-07-23 16:15:34 -0700

    Added mongo memory database.
    Signed-off-by: Jeremy Castillo <jeremy.c@accessorypower.com>

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var fs = require("fs");
-var socketIo = require("socket.io");
-var cookieParser = require("cookie-parser");
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var socketIo = require('socket.io');
+var cookieParser = require('cookie-parser');
 
 // Construct a config object based on json formatted settings.
-const contents = fs.readFileSync(require.resolve("./config.json"));
+const contents = fs.readFileSync(require.resolve('./config.json'));
 var config = JSON.parse(contents);
 
 var app = express();
-const http = require("http").Server(app);
-var { Board } = require("./models/board");
+const http = require('http').Server(app);
+var { Board } = require('./models/board');
 
 // Place the config object onto our request,
 // making it easily accessible to our API routes.
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // Routes are handled within each respective model
-const modelsDir = "./controllers/";
+const modelsDir = './controllers/';
 fs.readdir(modelsDir, (err, files) => {
   files.forEach((file) => {
     // eslint-disable-next-line import/no-dynamic-require
@@ -30,37 +30,37 @@ fs.readdir(modelsDir, (err, files) => {
   });
 
   const io = socketIo(http);
-  io.on("connection", (socket) => {
-    console.log("New client connected");
-    socket.on("gameJoined", ({ gameCode }) => {
+  io.on('connection', (socket) => {
+    console.log('New client connected');
+    socket.on('gameJoined', ({ gameCode }) => {
       console.log(`New player joined the game with code ${gameCode}`);
-      io.sockets.in(gameCode).emit("gameJoined");
+      io.sockets.in(gameCode).emit('gameJoined');
       socket.join(gameCode);
     });
 
     // INCOMPLETE!!!!
-    socket.on("movePiece", ({ gameCode, piece, cell }) => {
+    socket.on('movePiece', ({ gameCode, piece, cell }) => {
       console.log(`Got piece moved request! ${gameCode}`);
       Board.findOne({ game_code: gameCode }).then((foundBoard) => {
         foundBoard.movePiece(piece, cell);
-        foundBoard.markModified("matrix");
+        foundBoard.markModified('matrix');
         foundBoard.save().then(() => {
-          console.log("Sending response back to room.");
-          io.sockets.in(gameCode).emit("pieceMoved");
+          console.log('Sending response back to room.');
+          io.sockets.in(gameCode).emit('pieceMoved');
         });
       });
     });
 
-    socket.on("disconnect", () => {
+    socket.on('disconnect', () => {
       console.log(`Client disconnected from game: ${socket.rooms[0]}`);
-      socket.to(socket.rooms[0]).emit("gameDisconnected");
+      socket.to(socket.rooms[0]).emit('gameDisconnected');
       socket.leaveAll();
     });
   });
 
   http.listen(3030, () => {
     // eslint-disable-next-line no-console
-    console.log("Listening on port 3030");
+    console.log('Listening on port 3030');
   });
 });
 
--- system/mongoose.js
+++ system/mongoose.js
@@ -1,27 +1,44 @@
 /* eslint-disable no-undef */
 var mongoose = require('mongoose');
+var fs = require('fs');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/BrowserChessDB', {
-  useNewUrlParser: true
+const mongod = new MongoMemoryServer();
+
+// Construct a config object based on json formatted settings.
+const contents = fs.readFileSync(require.resolve('../config.json'));
+var config = JSON.parse(contents);
+
+let uriBuilder = new Promise((resolve, reject) => {
+  let uri = 'mongodb://localhost:27017/BrowserChessDB';
+  if (config.noMongo) {
+    mongod.getConnectionString().then((response) => resolve(response));
+    console.log(uri);
+  } else {
+    resolve(uri);
+  }
 });
 
-if (typeof before !== typeof undefined) {
-  before((done) => {
-    console.log('Connecting to test DB');
-    mongoose
-      .connect('mongodb://localhost:27017/BrowserChessDBTest', {
-        useNewUrlParser: true
-      })
-      .then(() => {
-        console.log('Connected to test DB.');
-        done();
-      });
-  });
+mongoose.Promise = global.Promise;
+uriBuilder.then((uri) => {
+  mongoose.connect(uri, { useNewUrlParser: true });
+  if (typeof before !== typeof undefined) {
+    before((done) => {
+      console.log('Connecting to test DB');
+      mongoose
+        .connect('mongodb://localhost:27017/BrowserChessDBTest', {
+          useNewUrlParser: true
+        })
+        .then(() => {
+          console.log('Connected to test DB.');
+          done();
+        });
+    });
 
-  beforeEach((done) => {
-    mongoose.connection.db.dropDatabase().then(() => done());
-  });
-}
+    beforeEach((done) => {
+      mongoose.connection.db.dropDatabase().then(() => done());
+    });
+  }
+});
 
 module.exports = { mongoose };
