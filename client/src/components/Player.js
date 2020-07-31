b650d0c65a15bdc8207cdcf13612a366a02d4b0f
Jeremy Castillo <jcast029@ucr.edu>
2020-07-30 21:49:29 -0700

    Fixed a bug with turn based movement.
    Signed-off-by: Jeremy Castillo <jcast029@ucr.edu>

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,28 +1,32 @@
-import React from 'react';
-import { Typography, Card } from '@material-ui/core';
+import React from "react";
+import { Typography, Card } from "@material-ui/core";
 
 const Player = (props) => {
-  const playerName = props.player ? props.player.name : 'No Player';
+  const playerName = props.player ? props.player.name : "No Player";
   const turnMessage =
     props.board &&
     props.player &&
     props.board.player_1 &&
     props.board.player_2 &&
     props.board.turn === props.player.color
-      ? 'Your Turn'
-      : '';
+      ? "Your Turn"
+      : "";
+  try {
+    console.log(props.player.color);
+    console.log(props.board.turn);
+  } catch (err) {}
 
   return (
-    <div className='container'>
-      <Card className='card'>
-        <div className='card-header'>
+    <div className="container">
+      <Card className="card">
+        <div className="card-header">
           <Typography
-            component='h4'
-            variant='h4'
-            className='card-header-title has-text-centered is-block'
+            component="h4"
+            variant="h4"
+            className="card-header-title has-text-centered is-block"
           >
             {playerName}
-            <Typography variant='subtitle1'>{turnMessage}</Typography>
+            <Typography variant="subtitle1">{turnMessage}</Typography>
           </Typography>
         </div>
       </Card>
